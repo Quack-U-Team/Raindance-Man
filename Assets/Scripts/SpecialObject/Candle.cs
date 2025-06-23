268725bdc345aeaa07ad13cf09ae9418abf7965d
Giacomo Calì <caligiacomo0@gmail.com>
2025-06-23 16:36:24 +0200

    Implementazione base candela
    Funziona ma mancano le animazioni

--- Assets/Scripts/SpecialObject/Candle.cs
+++ Assets/Scripts/SpecialObject/Candle.cs
@@ -3,20 +3,25 @@ using UnityEngine.Rendering.Universal;
 
 public class Candle : MonoBehaviour
 {
+    PlayerMovement playerMovement = null;
+    public float removeDebuffTimer = 2f;
+
+    void RemoveDebuff()
+    {
+        playerMovement.ansia = false;
+        playerMovement.depressione = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             print("entered heal area");
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            print("exited heal area");
+            if (collision.GetComponent<PlayerMovement>() != null) 
+            { 
+                playerMovement = collision.GetComponent<PlayerMovement>();
+                Invoke("RemoveDebuff", removeDebuffTimer);
+            }
         }
     }
 }
