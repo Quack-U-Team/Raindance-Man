5871b64d5db051e18417f9b444e2546a3bcede76
Giacomo Calì <caligiacomo0@gmail.com>
2025-06-23 13:52:50 +0200

    Yada yada

--- Assets/Scripts/SpecialObject/Candle.cs
+++ Assets/Scripts/SpecialObject/Candle.cs
@@ -6,22 +6,17 @@ public class Candle : MonoBehaviour
     private Light2D candleLight;
     public float flickerAmount = 0.2f;
     public float flickerSpeed = 0.3f;
-    private float baseIntensity;
 
     void Start()
     {
         candleLight = GetComponent<Light2D>();
-        if (candleLight != null)
-        {
-            baseIntensity = candleLight.intensity;
-        }
     }
 
     void Update()
     {
         if (candleLight != null)
         {
-            candleLight.intensity = baseIntensity + Mathf.Sin(Time.time * flickerSpeed) * flickerAmount;
+            candleLight.intensity *= Mathf.Sin(Time.time * flickerSpeed) * flickerAmount;
         }
     }
 }
